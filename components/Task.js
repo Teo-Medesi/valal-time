59fc694c4a905c52960dbb9574aa99c5e88f812d
Teo-Medesi <teo.medesi@gmail.com>
2023-08-31 17:42:13 +0200

    make state management great again

--- components/Task.js
+++ components/Task.js
@@ -1,8 +1,20 @@
+import { useBranch } from '@/context/BranchContext';
+import { useSession } from '@/context/SessionContext';
 import React from 'react'
 
-const Task = () => {
+const Task = ({ task, isSelected }) => {
+  const { setSelectedTask, selectedBranch, selectedProject } = useBranch();
+
+  const handleClick = () => {
+    setSelectedTask(task)
+  }
+
   return (
-    <div>Task</div>
+    <div onClick={handleClick} className={`btn btn-primary !text-black flex justify-between ${isSelected ? "btn-primary" : "btn-outline"}`}>
+      <p>{task?.name}</p>
+      <p>{task?.description}</p>
+      <p>{task?.total_time_tracked || "0h 0m"}</p>
+    </div>
   )
 }
 
