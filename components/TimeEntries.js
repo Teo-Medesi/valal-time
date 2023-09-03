726d49b43b056bd506ef57cc3a566951117af947
Teo-Medesi <teo.medesi@gmail.com>
2023-09-03 22:13:07 +0200

    minor ui change

--- components/TimeEntries.js
+++ components/TimeEntries.js
@@ -6,7 +6,7 @@ const TimeEntries = () => {
     const { timeEntries, currentTimeEntry } = useBranch();
     
   return (
-    <div className="flex flex-col w-full gap-4">
+    <div className={`flex flex-col w-full gap-4 ${timeEntries?.length === 0 && "hidden"}`}>
         {timeEntries?.map(element => <TimeEntry entry={element} />)}        
     </div>
   )
