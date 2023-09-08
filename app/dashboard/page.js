36f94a577ef8cb41554df45385152d1ff4911da8
Teo-Medesi <teo.medesi@gmail.com>
2023-09-08 14:53:11 +0200

    mobile hero header

---
+++ app/dashboard/page.js
@@ -0,0 +1,23 @@
+import { BranchSelectBox, ProjectSelectBox, TaskSelectBox, TasksTodo, Timer, DateSelect, TimeEntries } from "@/components";
+import supabase from "@/lib/supabase-server.config";
+
+export default async function Home() {
+  const { data: { user } } = await supabase.auth.getUser();
+
+  return (
+    <div className="flex md:justify-center w-full min-h-screen">
+      <div className="w-full md:w-3/4 flex flex-col gap-16 items-center padding-y padding-x">
+        <div className="flex flex-col gap-4 md:gap-0 md:flex-row justify-between w-full">
+          <DateSelect className="flex md:hidden"/>
+          <BranchSelectBox />
+          <DateSelect className="hidden md:flex"/>
+          <ProjectSelectBox />
+        </div>
+        <Timer />
+        <TaskSelectBox />
+        <TimeEntries />
+        <TasksTodo />
+      </div>
+    </div>
+  )
+}
--- app/page.js
+++ app/page.js
@@ -1,22 +1,24 @@
-import { BranchSelectBox, ProjectSelectBox, TaskSelectBox, TasksTodo, Timer, DateSelect, TimeEntries } from "@/components";
-import supabase from "@/lib/supabase-server.config";
+import Logo from "@/public/logo.svg"
 
 export default async function Home() {
-  const { data: { user } } = await supabase.auth.getUser();
 
   return (
-    <div className="flex md:justify-center w-full min-h-screen">
-      <div className="w-full md:w-3/4 flex flex-col gap-16 items-center padding-y padding-x">
-        <div className="flex flex-col gap-4 md:gap-0 md:flex-row justify-between w-full">
-          <DateSelect className="flex md:hidden"/>
-          <BranchSelectBox />
-          <DateSelect className="hidden md:flex"/>
-          <ProjectSelectBox />
+    <div className="flex absolute left-0 top-0 justify-center items-center w-full min-h-screen">
+      <div className="flex hero px-4 flex-col gap-16 text-center">
+        <svg className="animate-pulse" width="121" height="167" viewBox="0 0 121 167" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M1 1.5H119M1.5 2L1.5 26M119.5 1V25M1 165.5H119M119.5 142V166M1.5 142L1.5 166M1.60302 25.3959L119.104 142.897M119.354 24.3536L62.3536 81.3536M1.64645 142.646L58.6464 85.6464" stroke="url(#paint0_linear_1_37)" stroke-width="2" />
+          <defs>
+            <linearGradient id="paint0_linear_1_37" x1="60.25" y1="1" x2="60.25" y2="166" gradientUnits="userSpaceOnUse">
+              <stop stop-color="#1DB853" />
+              <stop offset="1" stop-color="#1DB990" stop-opacity="0.5" />
+            </linearGradient>
+          </defs>
+        </svg>
+        <div className="flex flex-col gap-2">
+          <h1 className="text-4xl bg-gradient-to-r from-primary to-secondary text-gradient">Don't Waste Time</h1>
+          <p className="text-neutral">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequuntur pariatur explicabo omnis quam vel animi optio velit voluptates ex assumenda.</p>
         </div>
-        <Timer />
-        <TaskSelectBox />
-        <TimeEntries />
-        <TasksTodo />
+        <button className="btn btn-secondary bg-gradient-to-r from-secondary to-primary">Start Tracking Time Now</button>
       </div>
     </div>
   )
